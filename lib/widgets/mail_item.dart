8c32e54c5d7f0b666b25b8208f5b4f753db8bea1
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-10-03 13:48:09 +0300

    working on meal card ui desgin

--- lib/widgets/mail_item.dart
+++ lib/widgets/mail_item.dart
@@ -13,6 +13,12 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      clipBehavior: Clip.hardEdge,
+      elevation: 2,
       child: InkWell(
         onTap: () {},
         child: Stack(
@@ -21,7 +27,38 @@ class MealItem extends StatelessWidget {
               placeholder: MemoryImage(kTransparentImage),
               image: NetworkImage(meal.imageUrl),
             ),
-            Text(meal.title)
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 6,
+                  horizontal: 44,
+                ),
+                color: Colors.black45,
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    const Row(
+                      children: [],
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
