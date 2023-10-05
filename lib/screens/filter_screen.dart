4997673e24142aaf2b668d3c72d1c7bc5646f08c
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-10-05 14:38:31 +0300

    add filters

--- lib/screens/filter_screen.dart
+++ lib/screens/filter_screen.dart
@@ -12,6 +12,11 @@ class FilterScreen extends StatefulWidget {
 }
 
 class _FilterScreenState extends State<FilterScreen> {
+  bool _glutenFreeFilter = false;
+  bool _veganFreeFilter = false;
+  bool _vegetarianFreeFilter = false;
+  bool _lactoseFreeFilter = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,8 +36,97 @@ class _FilterScreenState extends State<FilterScreen> {
           }
         },
       ),
-      body: const Column(
-        children: [],
+      body: Column(
+        children: [
+          SwitchListTile(
+            title: Text(
+              'Gluten-Free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include Gluten Free meals',
+              style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            onChanged: (bool value) {
+              setState(() {
+                _glutenFreeFilter = value;
+              });
+            },
+            value: _glutenFreeFilter,
+          ),
+          SwitchListTile(
+            title: Text(
+              'Vegan',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include Vegan meals',
+              style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            onChanged: (bool value) {
+              setState(() {
+                _veganFreeFilter = value;
+              });
+            },
+            value: _veganFreeFilter,
+          ),
+          SwitchListTile(
+            title: Text(
+              'Vegetarian',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include Vegetarian meals',
+              style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            onChanged: (bool value) {
+              setState(() {
+                _vegetarianFreeFilter = value;
+              });
+            },
+            value: _vegetarianFreeFilter,
+          ),
+          SwitchListTile(
+            title: Text(
+              'Lactose-Free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include Lactose Free meals',
+              style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            onChanged: (bool value) {
+              setState(() {
+                _lactoseFreeFilter = value;
+              });
+            },
+            value: _lactoseFreeFilter,
+          ),
+        ],
       ),
     );
   }
